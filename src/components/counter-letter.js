0665e01965f5607fb4bafc2b8cd34cd6fa67c3c2
Hamidullah <hamidullahh754@gmail.com>
2024-12-20 20:32:24 +0430

    changes

--- src/components/counter-letter.js
+++ src/components/counter-letter.js
@@ -1,30 +1,15 @@
-// function CounterLetter(text, letter) {
-//   return text.split(letter).length - 1;
-// }
-
 function CounterLetter(text, letter) {
+  // return text.split(letter).length - 1;
+
   let count = 0;
+
   for (let e of text) {
-    if (e === letter) {
-      count++;
-    }
+    if (e === letter) count++;
   }
+
   return count;
 }
 
-console.log(CounterLetter('sadfgfgsdfghfgfdghfdsghghshfghhsfdghhghshdfghhghds', 'h'));
+console.log(CounterLetter('hhhhhhhdddd', 'h'));
 
-export default CounterLetter
-
-
-let array = [10, 20, 30, 40, 50];
-
-for (let number of array) {
-  console.log(number);   // خروجی: 10, 20, 30, 40, 50
-}
-
-let person = { name: "Hamid", age: 21, city: "Herat" };
-
-for (let key in person) {
-  console.log(key, person[key]);    // خروجی: name Hamid, age 21, city Herat
-}
+export default CounterLetter;
